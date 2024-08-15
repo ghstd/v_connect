01fc3e138d3e5d58bcf557b2c1222b251ea6867b
AntonRm <tworkn1@gmail.com>
2024-08-15 18:25:11 +0300

    change html on root page

--- app/controllers/websocket_controller.rb
+++ app/controllers/websocket_controller.rb
@@ -1,9 +1,6 @@
 class WebsocketController < ApplicationController
   def index
-    p '==============================='
-    p request.ip
-    p '==============================='
-    html_content = "<h1>Dynamic HTML Content</h1><p>Vlhm_connect service is working</p>"
+    html_content = "<h1>Vlhm_connect service is working!</h1>"
     render html: html_content.html_safe
   end
 end
